24943b113307195e26925f8466ea755efde1bc7f
Gavin D'mello <gavindmello@Gavins-MacBook-Pro.local>
2022-02-01 15:27:31 -0500

    added circular shifter for string

--- Assignment 1 - KWIC/KWIC-I1/src/App.java
+++ Assignment 1 - KWIC/KWIC-I1/src/App.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
+
 public class App {
 
-  public static void main(String[] args) throws Exception {}
+  public static void main(String[] args) throws Exception {
+    CircularShifter cs = new CircularShifter("My name is Gavin D'mello");
+    ArrayList<String> csResult = cs.shift();
+    System.out.println(csResult);
+  }
 }
--- Assignment 1 - KWIC/KWIC-I1/src/CircularShifter.java
+++ Assignment 1 - KWIC/KWIC-I1/src/CircularShifter.java
@@ -1,1 +1,25 @@
-public class CircularShifter {}
+import java.util.ArrayList;
+
+public class CircularShifter {
+  String[] input;
+
+  CircularShifter(String string) {
+    this.input = string.split(" ");
+  }
+
+  public ArrayList<String> shift() {
+    ArrayList<String> shiftedStrings = new ArrayList<String>();
+    shiftedStrings.add(String.join(" ", this.input));
+    for (int i = 1; i < this.input.length; i++) {
+      String[] tempArr = this.input;
+      String tempStr = tempArr[0];
+      for (int j = 0; j < tempArr.length - 1; j++) {
+        tempArr[j] = tempArr[j + 1];
+      }
+      tempArr[tempArr.length - 1] = tempStr;
+      this.input = tempArr;
+      shiftedStrings.add(String.join(" ", this.input));
+    }
+    return shiftedStrings;
+  }
+}
