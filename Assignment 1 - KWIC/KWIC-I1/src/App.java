1b334b1a850012bf98450b9a9d18cbc093039c13
Gavin D'mello <gavindmello@Gavins-MacBook-Pro.local>
2022-02-02 14:36:12 -0500

    breaking down of individual functions

--- Assignment 1 - KWIC/KWIC-I1/src/App.java
+++ Assignment 1 - KWIC/KWIC-I1/src/App.java
@@ -4,7 +4,7 @@ public class App {
 
   public static void main(String[] args) throws Exception {
     CircularShifter cs = new CircularShifter("My name is Gavin D'mello");
-    ArrayList<String> csResult = cs.shift();
+    ArrayList<String> csResult = cs.generateAllStrings();
     System.out.println(csResult);
   }
 }
--- Assignment 1 - KWIC/KWIC-I1/src/CircularShifter.java
+++ Assignment 1 - KWIC/KWIC-I1/src/CircularShifter.java
@@ -7,19 +7,22 @@ public class CircularShifter {
     this.input = string.split(" ");
   }
 
-  public ArrayList<String> shift() {
+  public ArrayList<String> generateAllStrings() {
     ArrayList<String> shiftedStrings = new ArrayList<String>();
-    shiftedStrings.add(String.join(" ", this.input));
-    for (int i = 1; i < this.input.length; i++) {
-      String[] tempArr = this.input;
-      String tempStr = tempArr[0];
-      for (int j = 0; j < tempArr.length - 1; j++) {
-        tempArr[j] = tempArr[j + 1];
-      }
-      tempArr[tempArr.length - 1] = tempStr;
-      this.input = tempArr;
-      shiftedStrings.add(String.join(" ", this.input));
+    for (int i = 0; i < this.input.length; i++) {
+      shiftedStrings.add(String.join(" ", rightShifter()));
     }
     return shiftedStrings;
+  }
+
+  private String[] rightShifter() {
+    String[] tempArr = this.input;
+    String tempStr = tempArr[0];
+    for (int j = 0; j < tempArr.length - 1; j++) {
+      tempArr[j] = tempArr[j + 1];
+    }
+    tempArr[tempArr.length - 1] = tempStr;
+    this.input = tempArr;
+    return this.input;
   }
 }
