9ac9f87f5a75799f524184b033ff0a5c7d94ce13
Kameron Jusseaume <kamjuss12198@gmail.com>
2022-02-06 10:50:34 -0500

    Alphabetizer Documentation
    Added documentation to the alphabetizer as required from the assignment instructions

--- Assignment 1 - KWIC/KWIC-I1/src/Alphabetizer.java
+++ Assignment 1 - KWIC/KWIC-I1/src/Alphabetizer.java
@@ -1,12 +1,17 @@
+/*Kameron Jusseaume
+CIS 532-01 Assignment 1
+Implementation 1 Alphabetizer */
+
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Alphabetizer {
 
-  public ArrayList<String> alphabetize(
-    ArrayList<ArrayList<String>> circularShiftsArr
-  ) {
+  public ArrayList<String> alphabetize(ArrayList<ArrayList<String>> circularShiftsArr) 
+  {
+    //Create a new arraylist to hold the alphabetized list
     ArrayList<String> result = new ArrayList<String>();
+
     /* This was used for testing
         String array[] =  {"My name is Gavin D'mello", 
         "name is Gavin D'mello My", 
@@ -17,6 +22,8 @@ public class Alphabetizer {
         System.out.printf("Modified arr[] : \n%s\n\n",
                           Arrays.toString(array));
         */
+
+    //Sort each element of the arraylist while also accounting for multiple different input strings
     for (int i = 0; i < circularShiftsArr.size(); i++) {
       Collections.sort(circularShiftsArr.get(i), String.CASE_INSENSITIVE_ORDER);
       for (int p = 0; p < circularShiftsArr.get(i).size(); p++) {
@@ -24,6 +31,7 @@ public class Alphabetizer {
       }
     }
 
+    //return the ordered list to be stored and output by the main module
     return result;
   }
 }
