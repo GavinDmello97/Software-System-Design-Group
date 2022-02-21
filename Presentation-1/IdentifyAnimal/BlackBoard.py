ffc16989bfdac8696c76470b514ca72df6f46b2a
Gavin D'mello <gavindmello@Gavins-MacBook-Pro.local>
2022-02-20 22:49:32 -0500

    added main and blackboard config

--- Presentation-1/IdentifyAnimal/BlackBoard.py
+++ Presentation-1/IdentifyAnimal/BlackBoard.py
@@ -1,9 +1,15 @@
 class Blackboard:
     def __init__(self):
         self.experts = []
-        self.commonState = {}
         self.facts = []
-        self.conclusion = ""
+        self.result = ""
+
+    def addFacts(self, facts):
+        self.facts = facts
+        print("\nThe following facts were added to the blackboard:")
+        print(self.facts)
+        print("\n")
 
     def addExpert(self, expert):
         self.experts.append(expert)
+        print("Expert assigned to the blackboard\n")
